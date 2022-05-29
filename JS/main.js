d0c8bcb8f0d59a6357ba3e8618fc844aa3cde574
Samuel Bassham <42912617+bassham12@users.noreply.github.com>
2022-05-29 02:31:54 -0500

    created event handlers for the nav bar

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,51 @@
-menuIcon = document.getElementById("menu-open");
+const featureDropdown = document.getElementById("features");
+let featureCounter = 0;
+let companyCounter = 0;
 
+featureDropdown.addEventListener("click", () => {
 
-menuIcon.addEventlistener("onclick", ()=> {
-    
-})
+    const featureDownArrow = document.getElementById("features-down-arrow");
+    const featureUpArrow = document.getElementById("features-up-arrow");
+    const featureDropList = document.getElementById("features-droplist");
+    featureCounter += 1;
+
+    if (featureCounter == 1) {
+        featureDownArrow.style.visibility="hidden";
+        featureUpArrow.style.visibility="visible";
+        featureDropList.style.visibility="visible";
+    }
+    else {
+        featureDropList.style.visibility="hidden";
+        featureUpArrow.style.visibility="hidden";
+        featureDownArrow.style.visibility="visible";
+        
+    }
+
+    if (featureCounter > 1) featureCounter = 0;
+    console.log(featureCounter);
+});
+
+const companyDropdown = document.getElementById("company");
+
+companyDropdown.addEventListener("click", () => {
+
+    const companyDownArrow = document.getElementById("company-down-arrow");
+    const companyUpArrow = document.getElementById("company-up-arrow");
+    const companyDropList = document.getElementById("company-droplist");
+    companyCounter += 1;
+
+    if (companyCounter == 1) {
+        companyDownArrow.style.visibility="hidden";
+        companyUpArrow.style.visibility="visible";
+        companyDropList.style.visibility="visible";
+    }
+    else {
+        companyDropList.style.visibility="hidden";
+        companyUpArrow.style.visibility="hidden";
+        companyDownArrow.style.visibility="visible";
+        
+    }
+
+    if (companyCounter > 1) companyCounter = 0;
+    console.log(companyCounter);
+});
